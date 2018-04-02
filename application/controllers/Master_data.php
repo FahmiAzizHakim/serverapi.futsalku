715464ea87fee0a7368b411de0295a49f1c9e328
Munawar Holil <holilmunawar026@gmail.com>
2018-04-02 15:04:59 +0700

    Ganti nama controllers/Kontak.php >>> controllers/Master_data.php

--- application/controllers/Master_data.php
+++ application/controllers/Master_data.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
-class Kontak extends REST_Controller {
+class Master_data extends REST_Controller {
 
     function __construct($config = 'rest') {
         parent::__construct($config);
